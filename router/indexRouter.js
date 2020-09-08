efe9af9dd5b0813d3687230841ad1fdf0e533030
KDM7 <jeremiah_bool@dlsu.edu.ph>
2020-09-08 18:52:37 +0800

    Update indexRouter.js

--- router/indexRouter.js
+++ router/indexRouter.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const router = express();
 const controller = require('../controller/index');
+
+router.get('/', controller.getHome);
+router.get('/registration', controller.getRegister);
+router.get('/login', controller.getLogin);
+router.get('/account', controller.getAccount);
