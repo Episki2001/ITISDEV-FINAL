e439c57882566fbbedc48adc37b08a55e12b5d8b
Andre Garcia <61376752+Episki2001@users.noreply.github.com>
2020-09-08 21:51:10 +0800

    Update indexRouter.js

--- router/indexRouter.js
+++ router/indexRouter.js
@@ -3,6 +3,5 @@ const router = express();
 const controller = require('../controller/index');
 
 router.get('/', controller.getHome);
-router.get('/registration', controller.getRegister);
-router.get('/login', controller.getLogin);
-router.get('/account', controller.getAccount);
+
+module.exports = router;
