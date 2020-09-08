c11a088c3edd429d13eef31ce59ccb6207b120ad
KDM7 <jeremiah_bool@dlsu.edu.ph>
2020-09-08 16:16:37 +0800

    Create purchasedb.js

---
+++ model/purchasedb.js
@@ -0,0 +1,34 @@
+// import module `mongoose`
+var mongoose = require('mongoose');
+
+// defines the schema for collection `suppliers`
+var PurchaseSchema = new mongoose.Schema({
+    purchaseID: {
+        type: Number,
+        required: true
+    },
+    amountPaid: {
+        type: Number,
+        required: true
+    },
+    datePurchased: {
+        type: Date,
+        required: true
+    },
+    totalCost: {
+        type: Number,
+        required: true
+    },
+    managerID: {
+        type: Number,
+        required: true
+    }
+});
+
+/*
+    exports a mongoose.model object based on `UserSchema` (defined above)
+    when another script exports from this file
+    This model executes CRUD operations
+    to collection `users` -> plural of the argument `User`
+*/
+module.exports = mongoose.model('Purchase', PurchaseSchema);
