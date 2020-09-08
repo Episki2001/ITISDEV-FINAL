b8b165fcc452f86aaad0dacb427380b997a54a5c
KDM7 <jeremiah_bool@dlsu.edu.ph>
2020-09-08 16:30:02 +0800

    Create discountdb.js

---
+++ model/discountdb.js
@@ -0,0 +1,46 @@
+// import module `mongoose`
+var mongoose = require('mongoose');
+
+// defines the schema for collection `suppliers`
+var DiscountSchema = new mongoose.Schema({
+    discountID: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    sellingPrice: {
+        type: Number,
+        required: true
+    },
+    discount: {
+        type: Number,
+        required: true
+    },
+    total: {
+        type: Number,
+        required: true
+    },
+    dateSold: {
+        type: Number,
+        required: true
+    },
+    productID: {
+        type: Number,
+        required: true
+    },
+    userID: {
+        type: Number,
+        required: true
+    }
+});
+
+/*
+    exports a mongoose.model object based on `UserSchema` (defined above)
+    when another script exports from this file
+    This model executes CRUD operations
+    to collection `users` -> plural of the argument `User`
+*/
+module.exports = mongoose.model('Discount', DiscountSchema);
