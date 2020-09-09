ba40047da5d9cf17ad0ba11386ee8849c3c6fcfb
KDM7 <jeremiah_bool@dlsu.edu.ph>
2020-09-09 21:54:05 +0800

    db

--- model/productdb.js
+++ model/productdb.js
@@ -13,7 +13,6 @@ var db = mongoose.connection;
 var ProductSchema = new mongoose.Schema({
     productID: { type: Number, required: true },
     productName: { type: String, required: true },
-    productType: { type: String, required: true },
     currentStock: { type: String, required: true },
     sellingPrice: { type: Number, required: true },
     purchasePrice: { type: Number, required: true },
