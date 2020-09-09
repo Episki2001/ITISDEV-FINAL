8b748c3d3dc727fe65a62550e1d5c015e251a08e
Andre Garcia <61376752+Episki2001@users.noreply.github.com>
2020-09-09 15:47:52 +0800

    Update indexRouter.js

--- router/indexRouter.js
+++ router/indexRouter.js
@@ -4,9 +4,9 @@ const controller = require('../controller/index');
 
 
 router.get('/', controller.getLogin);
-router.get('/a/discrepancy', controller.getAdiscrepancy);
-router.get('/a/editProduct', controller.getAeditProduct);
-router.get('/a/editProfile', controller.getAeditProfile);
+router.get('/a/discrepancy/*', controller.getAdiscrepancy);
+router.get('/a/product/*', controller.getAeditProduct);
+router.get('/a/user/*', controller.getAeditProfile);
 router.get('/a/editSupplier', controller.getAeditSupplier);
 router.get('/a/MDgoods', controller.getAMDgoods);
 router.get('/a/newProducts', controller.getAnewProducts);
@@ -14,7 +14,7 @@ router.get('/a/newSupplier', controller.getAnewSupplier);
 router.get('/a/newUser', controller.getAnewUser);
 router.get('/a/products', controller.getAproducts);
 router.get('/a/purchases', controller.getApurchases);
-router.get('/a/suppliers', controller.getAsuppliers);
+router.get('/a/suppliers/*', controller.getAsuppliers);
 router.get('/a/users', controller.getAusers);
 
 module.exports = router;
