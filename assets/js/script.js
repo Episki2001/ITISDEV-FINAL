82b5b3c5a937fb5d9907b561410f1a72baeb2d83
Andre Garcia <61376752+Episki2001@users.noreply.github.com>
2020-09-12 16:28:51 +0800

    added validation for login

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,22 +8,29 @@ $(document).ready(function() {
         var user = $('#id').val();
         var pass = $('#pword').val();
 
+        if (validator.isEmpty(user)) {
+            alert('lol');
+        }
+        if (validator.isEmpty(pass)) {
+            alert('Please fill in all fields');
+        }
 
-        $.post('/', { user: user, pass: pass }, function(result) {
-            switch (result.status) {
-                case 200:
-                    {
-                        window.location.href = '/a/suppliers';
-                        break;
-                    }
-                case 401:
-                case 500:
-                    {
-                        alert('case 500' + result.msg);
-                        break;
-                    }
-            }
-        });
-
+        if (!validator.isEmpty(user) && !validator.isEmpty(pass)) {
+            $.post('/', { user: user, pass: pass }, function(result) {
+                switch (result.status) {
+                    case 200:
+                        {
+                            window.location.href = '/a/suppliers';
+                            break;
+                        }
+                    case 401:
+                    case 500:
+                        {
+                            alert('case 500' + result.msg);
+                            break;
+                        }
+                }
+            });
+        }
     });
 });
